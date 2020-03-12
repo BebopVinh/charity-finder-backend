e0d6cbace0e8fdb4670cb88c6905b596ebaf295d
bebopvinh <vinhdo07@gmail.com>
2020-03-11 23:35:21 -0700

    Initialize Node Express with npm

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+const fetch = require("node-fetch")
+
+const sample = {
+	greetings: "How are you?"
+}
+
+const { apiKey } = require("./config")
+
+app.get("/", (req, res) => res.send(sample))
+
+app.get("/organizations", (req, res) => {
+	const url = `https://api.globalgiving.org/api/public/orgservice/all/organizations.json?api_key=${apiKey}`
+	fetch(url)
+		.then(response => response.json())
+		.then(data => res.send(data))
+})
+
+app.listen(port, () => console.log(`Backend server is up on port ${port}`))
